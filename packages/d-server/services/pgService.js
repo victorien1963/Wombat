22fb394838d8153152ed27239e5d30a090f47def
selikastraying <selikastraying@gmail.com>
2024-03-24 10:51:17 +0800

    fix email

--- packages/d-server/services/pgService.js
+++ packages/d-server/services/pgService.js
@@ -54,11 +54,11 @@ const initData = async () => {
     console.log('Initing data')
     const hashed = await hash(process.env.ADMIN_PASSWORD)
     try {
-      await initialCheck('SELECT * FROM users WHERE user_id = $1', ['1'], 'INSERT INTO users(user_id, name, email, password, setting, created_on) VALUES($1, $2, $3, $4, $5, current_timestamp) ON CONFLICT DO NOTHING', ['1', 'dAIVinci', process.env.ADMIN_EMAIL, hashed, { admin: true }])
-      await initialCheck('SELECT * FROM users WHERE user_id = $1', ['2'], 'INSERT INTO users(user_id, name, email, password, setting, created_on) VALUES($1, $2, $3, $4, $5, current_timestamp) ON CONFLICT DO NOTHING', ['2', 'dAIVinci02', `daivinci02@wavenet.com.tw`, hashed, { admin: true }])
-      await initialCheck('SELECT * FROM users WHERE user_id = $1', ['3'], 'INSERT INTO users(user_id, name, email, password, setting, created_on) VALUES($1, $2, $3, $4, $5, current_timestamp) ON CONFLICT DO NOTHING', ['3', 'dAIVinci03', `daivinci03@wavenet.com.tw`, hashed, { admin: true }])
-      await initialCheck('SELECT * FROM users WHERE user_id = $1', ['4'], 'INSERT INTO users(user_id, name, email, password, setting, created_on) VALUES($1, $2, $3, $4, $5, current_timestamp) ON CONFLICT DO NOTHING', ['4', 'dAIVinci04', `daivinci04@wavenet.com.tw`, hashed, { admin: true }])
-      await initialCheck('SELECT * FROM users WHERE user_id = $1', ['5'], 'INSERT INTO users(user_id, name, email, password, setting, created_on) VALUES($1, $2, $3, $4, $5, current_timestamp) ON CONFLICT DO NOTHING', ['5', 'dAIVinci05', `daivinci05@wavenet.com.tw`, hashed, { admin: true }])
+      await initialCheck('SELECT * FROM users WHERE user_id = $1', ['1'], 'INSERT INTO users(user_id, name, email, password, setting, created_on) VALUES($1, $2, $3, $4, $5, current_timestamp) ON CONFLICT DO NOTHING', ['1', 'Wombat', process.env.ADMIN_EMAIL, hashed, { admin: true }])
+      await initialCheck('SELECT * FROM users WHERE user_id = $1', ['2'], 'INSERT INTO users(user_id, name, email, password, setting, created_on) VALUES($1, $2, $3, $4, $5, current_timestamp) ON CONFLICT DO NOTHING', ['2', 'Wombat02', `Wombat02@wavenet.com.tw`, hashed, { admin: true }])
+      await initialCheck('SELECT * FROM users WHERE user_id = $1', ['3'], 'INSERT INTO users(user_id, name, email, password, setting, created_on) VALUES($1, $2, $3, $4, $5, current_timestamp) ON CONFLICT DO NOTHING', ['3', 'Wombat03', `Wombat03@wavenet.com.tw`, hashed, { admin: true }])
+      await initialCheck('SELECT * FROM users WHERE user_id = $1', ['4'], 'INSERT INTO users(user_id, name, email, password, setting, created_on) VALUES($1, $2, $3, $4, $5, current_timestamp) ON CONFLICT DO NOTHING', ['4', 'Wombat04', `Wombat04@wavenet.com.tw`, hashed, { admin: true }])
+      await initialCheck('SELECT * FROM users WHERE user_id = $1', ['5'], 'INSERT INTO users(user_id, name, email, password, setting, created_on) VALUES($1, $2, $3, $4, $5, current_timestamp) ON CONFLICT DO NOTHING', ['5', 'Wombat05', `Wombat05@wavenet.com.tw`, hashed, { admin: true }])
     } catch (e) {
       console.log('admin user already exists.')
       console.log(e)
